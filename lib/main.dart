6c9c659c39bcd5bbdd39dfabae4ccac9bf1afc34
Akshay Pathania <akshay.p@amakein.com>
2021-07-27 12:30:21 +0530

    update:firebase hosting , responsive framwork package

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:get/get.dart';
 import 'package:kindness/constants/app_routes.dart';
 import 'package:kindness/constants/themes.dart';
 import 'package:kindness/controllers/auth_controller.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
+import 'package:responsive_framework/utils/scroll_behavior.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +19,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      builder: (context, widget) => ResponsiveWrapper.builder(
+          BouncingScrollWrapper.builder(context, widget!),
+          maxWidth: 1200,
+          minWidth: 450,
+          defaultScale: true,
+          breakpoints: [
+            ResponsiveBreakpoint.resize(450, name: MOBILE),
+            ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+            ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+            ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+          ]),
       title: 'Kindness',
       debugShowCheckedModeBanner: false,
       theme: myTheme,
