baf458754305db3fc29f155f0181d1ffa8b21bf5
Akshay Pathania <akshay.p@amakein.com>
2021-08-25 14:15:03 +0530

    update:home screen is modified

--- lib/widgets/CustomCarouselSliderTiles.dart
+++ lib/widgets/CustomCarouselSliderTiles.dart
@@ -27,6 +27,7 @@ class CustomCarouselSliderTiles extends StatefulWidget {
 class _CustomCarouselSliderTilesState extends State<CustomCarouselSliderTiles> {
   late VideoPlayerController _controller;
   Future<void>? _initializeVideoPlayerFuture;
+
   @override
   void initState() {
     _controller = VideoPlayerController.network(widget.videoUrl);
@@ -85,44 +86,99 @@ class _CustomCarouselSliderTilesState extends State<CustomCarouselSliderTiles> {
         future: _initializeVideoPlayerFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: Stack(
-                children: [
-                  ColorFiltered(
-                      colorFilter: ColorFilter.mode(
-                          Colors.black.withOpacity(0.2), BlendMode.darken),
-                      child: VideoPlayer(_controller)),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    alignment: Alignment.topCenter,
-                    child: Text(
-                      title,
-                      style: GoogleFonts.poppins(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500),
-                    ),
+            return Stack(
+              children: [
+                ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.2), BlendMode.darken),
+                    child: CachedNetworkImage(
+                      height: Get.height * 0.2,
+                      width: Get.width,
+                      fit: BoxFit.cover,
+                      imageUrl:
+                          "https://firebasestorage.googleapis.com/v0/b/kindness-40bbd.appspot.com/o/files%2FkindnessInfo%2FWorld-Peace-Day-Sept-24th-2020.jpg?alt=media&token=9c5051d7-3903-4858-8584-71e1ac60a00e",
+                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  alignment: Alignment.topCenter,
+                  child: Text(
+                    title,
+                    style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
                   ),
-                  Center(
-                    child: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _controller.value.isPlaying
-                                ? _controller.pause()
-                                : _controller.play();
-                          });
-                        },
-                        icon: Icon(
+                ),
+                Center(
+                  child: IconButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            barrierDismissible: false,
+                            builder: (ctx) {
+                              return Dialog(
+                                elevation: 8,
+                                child: Stack(
+                                  children: [
+                                    Container(
+                                      height: Get.height * 0.4,
+                                      width: Get.width,
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Expanded(
+                                              child: AspectRatio(
+                                                  aspectRatio: _controller
+                                                      .value.aspectRatio,
+                                                  child: ColorFiltered(
+                                                      colorFilter:
+                                                          ColorFilter.mode(
+                                                              Colors
+                                                                  .black
+                                                                  .withOpacity(
+                                                                      0.2),
+                                                              BlendMode.darken),
+                                                      child: VideoPlayer(
+                                                          _controller)))),
+                                        ],
+                                      ),
+                                    ),
+                                    Positioned(
+                                        top: 0.0,
+                                        right: 0.0,
+                                        child: IconButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                _controller.value.isPlaying
+                                                    ? _controller.pause()
+                                                    : _controller.play();
+                                              });
+                                              Get.back();
+                                            },
+                                            icon: Icon(
+                                              Icons.clear,
+                                              color: Colors.white,
+                                            ))),
+                                  ],
+                                ),
+                              );
+                            });
+                        setState(() {
                           _controller.value.isPlaying
-                              ? Icons.pause
-                              : Icons.play_arrow,
-                          size: 40,
-                          color: Colors.white,
-                        )),
-                  )
-                ],
-              ),
+                              ? _controller.pause()
+                              : _controller.play();
+                        });
+                      },
+                      icon: Icon(
+                        _controller.value.isPlaying
+                            ? Icons.pause
+                            : Icons.play_arrow,
+                        size: 40,
+                        color: Colors.white,
+                      )),
+                )
+              ],
             );
           } else
             return Spinner();
